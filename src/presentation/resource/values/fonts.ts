a35e64834c65e0b52a91d4b126f75e6d9ad528e9
Vũ Trường Giang <giang.vu@alta.com.vn>
2021-06-29 11:38:21 +0700

    update

--- src/presentation/resource/values/fonts.ts
+++ src/presentation/resource/values/fonts.ts
@@ -1,3 +1,4 @@
+import {Platform} from 'react-native';
 export const Fonts = {
   Open_Sans_Bold: 'OpenSans-Bold',
   Open_Sans_Bold_Italic: 'OpenSans-BoldItalic',
@@ -6,7 +7,7 @@ export const Fonts = {
   OpenSans_Italic: 'OpenSans-Italic',
   OpenSans_Light: 'OpenSans-Light',
   Open_Sans_Light_Italic: 'OpenSans-LightItalic',
-  Open_Sans_Regular: 'OpenSans-Regular',
+  Open_Sans_Regular: Platform.select({})'OpenSans-Regular',;
   Open_Sans_Semibold: 'OpenSans-Semibold',
   Open_Sans_SemiboldItalic: 'OpenSans-SemiboldItalic',
 };
